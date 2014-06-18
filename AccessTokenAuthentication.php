dda49432f572cc783f442a6a6e53229a9c6ff6d2
deastis-s <deastis.salvatore@gmail.com>
2014-06-18 13:04:20 +0200

    text-to-speech implemendted

--- AccessTokenAuthentication.php
+++ AccessTokenAuthentication.php
@@ -1,29 +1,33 @@
 <?php
-class AccessTokenAuthentication {
-    /*
-     * Get the access token.
-     *
-     * @param string $grantType    Grant type.
-     * @param string $scopeUrl     Application Scope URL.
-     * @param string $clientID     Application client ID.
-     * @param string $clientSecret Application client ID.
-     * @param string $authUrl      Oauth Url.
-     *
-     * @return string.
-     */
-    function getTokens($grantType, $scopeUrl, $clientID, $clientSecret, $authUrl){
-        try {
+    
+class AccessTokenAuthentication 
+{
+    /********************************************************/
+    /*                                                      */
+    /* Get the access token.                                */
+    /*                                                      */
+    /* @param string $grantType    Grant type.              */
+    /* @param string $scopeUrl     Application Scope URL.   */
+    /* @param string $clientID     Application client ID.   */
+    /* @param string $clientSecret Application client ID.   */
+    /* @param string $authUrl      Oauth Url.               */
+    /*                                                      */
+    /* @return string.                                      */
+    /*                                                      */
+    /********************************************************/
+
+    function getTokens($grantType, $scopeUrl, $clientID, $clientSecret, $authUrl)
+    {
+        try 
+        {
             //Initialize the Curl Session.
             $ch = curl_init();
             //Create the request Array.
-            $paramArr = array (
-                 'grant_type'    => $grantType,
-                 'scope'         => $scopeUrl,
-                 'client_id'     => $clientID,
-                 'client_secret' => $clientSecret
-            );
+            $paramArr = array ('grant_type'=>$grantType, 'scope'=>$scopeUrl, 'client_id'=>$clientID, 'client_secret'=>$clientSecret);
+
             //Create an Http Query.//
             $paramArr = http_build_query($paramArr);
+            
             //Set the Curl URL.
             curl_setopt($ch, CURLOPT_URL, $authUrl);
             //Set HTTP POST Request.
@@ -34,25 +38,33 @@ class AccessTokenAuthentication {
             curl_setopt ($ch, CURLOPT_RETURNTRANSFER, TRUE);
             //CURLOPT_SSL_VERIFYPEER- Set FALSE to stop cURL from verifying the peer's certificate.
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            //Execute the  cURL session.
+            
+            //Execute the cURL session.
             $strResponse = curl_exec($ch);
+            
             //Get the Error Code returned by Curl.
             $curlErrno = curl_errno($ch);
-            if($curlErrno){
+            if($curlErrno)
+            {
                 $curlError = curl_error($ch);
                 throw new Exception($curlError);
             }
+
             //Close the Curl Session.
             curl_close($ch);
+            
             //Decode the returned JSON string.
             $objResponse = json_decode($strResponse);
-            if ($objResponse->error){
-                throw new Exception($objResponse->error_description);
+            if ($objResponse -> error)
+            {
+                throw new Exception($objResponse -> error_description);
             }
-            return $objResponse->access_token;
-        } catch (Exception $e) {
-            echo "Exception-".$e->getMessage();
-        }
+            return $objResponse -> access_token;
+        } catch (Exception $e) 
+          {
+              echo "Exception-".$e -> getMessage();
+          }
     }
 }
+
 ?>
--- translate.php
+++ translate.php
@@ -1,31 +1,44 @@
 <?php
 
+/* Initialize session for save the destination language value for text-to-speech */ 
+session_start();
+
+if (isset($_POST["dest_lang"]))
+{
+    $_SESSION["speech_lang"] = $_POST["dest_lang"];
+    
+}
+
+include 'HttpTranslator.php';
+include 'AccessTokenAuthentication.php';
+require 'credential.php';
+
 if('POST' == $_SERVER['REQUEST_METHOD'])
 {
-    include 'HttpTranslator.php';
-    include 'AccessTokenAuthentication.php';
-
+    /* Session variable for keep the languages selected */
+    /* Sets session variable for source_lang */
+    if (isset($_POST['source_lang']))
+    {
+        $_SESSION['source_lang'] = $_POST['source_lang'];
+    }
+    
+    /* Sets session variable for dest_lang */
+    if (isset($_POST['dest_lang']))
+    {
+        $_SESSION['dest_lang'] = $_POST['dest_lang'];
+    }
+  
+    
     try 
     {
-        //Client ID of the application.
-        $clientID = "";
-        //Client Secret key of the application.
-        $clientSecret = "";
-        //OAuth Url.
-        $authUrl = "https://datamarket.accesscontrol.windows.net/v2/OAuth2-13/";
-        //Application Scope Url
-        $scopeUrl = "http://api.microsofttranslator.com";
-        //Application grant type
-        $grantType = "client_credentials";
-
-        //Create the AccessTokenAuthentication object.
+        // Create the AccessTokenAuthentication object.
         $authObj = new AccessTokenAuthentication();
-        //Get the Access token.
-        $accessToken = $authObj->getTokens($grantType, $scopeUrl, $clientID, $clientSecret, $authUrl);
-        //Create the authorization Header string.
+        // Get the Access token.
+        $accessToken = $authObj -> getTokens($grantType, $scopeUrl, $clientID, $clientSecret, $authUrl);
+        // Create the authorization Header string.
         $authHeader = "Authorization: Bearer ". $accessToken;
 
-        //Set the parameters.
+        // Set the parameters.
         // Sets source language. $fromLanguage = variable, source_lang = name of textarea.
         $fromLanguage = $_POST["source_lang"];
         // Sets destination language. $toLanguage = variable, dest_lang = name of textarea.
@@ -45,7 +58,7 @@ if('POST' == $_SERVER['REQUEST_METHOD'])
         $translatorObj = new HTTPTranslator();
     
         //Get the curlResponse.
-        $curlResponse = $translatorObj->curlRequest($translateUrl, $authHeader);
+        $curlResponse = $translatorObj -> curlRequest($translateUrl, $authHeader);
     
         //Interprets a string of XML into an object.
         $xmlObj = simplexml_load_string($curlResponse);
@@ -59,18 +72,60 @@ if('POST' == $_SERVER['REQUEST_METHOD'])
           echo "Exception: " . $e->getMessage() . PHP_EOL;
       }
 }
+elseif ('GET' == $_SERVER['REQUEST_METHOD'])
+{
+    try 
+    {
+        //Create the AccessTokenAuthentication object.
+        $authObj = new AccessTokenAuthentication();
+        //Get the Access token.
+        $accessToken  = $authObj->getTokens($grantType, $scopeUrl, $clientID, $clientSecret, $authUrl);
+        //Create the authorization Header string.
+        $authHeader = "Authorization: Bearer ". $accessToken;
+        
+        //Set the params.
+        $speechStr = urlencode($_GET["transl_text"]);
+        $language = $_SESSION["speech_lang"];
+        $out = 'audio/mp3';
+        $params = "text=$speechStr&language=$language&format=$out";
+        
+        //HTTP Speak method URL.
+        $url = "http://api.microsofttranslator.com/V2/Http.svc/Speak?$params";
+        
+        //Set the Header Content Type.
+        header('Content-Type: audio/mp3');
+        header('Content-Disposition: attachment; filename = '.uniqid('SPC_').'.mp3');
+        
+        //Create the Translator Object.
+        $translatorObj = new HTTPTranslator();
+    
+        //Call the curlRequest.
+        $strResponse = $translatorObj -> curlRequest($url, $authHeader);
+        echo $strResponse;
+     
+        //echo ('<audio autoplay = "autoplay" >  <source src = "'.$strResponse.'" type = "audio/mp3" /> </audio> ');
+        
+
+} catch (Exception $e) 
+  {
+      echo "Exception: " . $e->getMessage() . PHP_EOL;
+  }
+}
+
+//session_destroy();
 
 ?>
 
 
 
 <!DOCTYPE html>
-<html lang="it">
+<html lang = "it">
 
 <head>
     <meta charset="utf-8" />
     <title>Microsoft Translator</title>
-    <link rel="stylesheet" href="css.css" />
+    <link rel = "stylesheet" href = "css.css" />
+    <script type = "text/javascript" src="functions_js.js"></script>
 </head>
 
 
@@ -81,56 +136,102 @@ if('POST' == $_SERVER['REQUEST_METHOD'])
         // Declaration of the array that contains the language of the translation.
         $languages = array("a"=>"Seleziona lingua", "it"=>"Italiano", "en"=>"Inglese", "ja"=>"Giapponese", "pt"=>"Portoghese", "es"=>"Spagnolo", "tr"=>"Turco",
         "zh-CHS"=>"Cinese Semplificato", "zh-CHT"=>"Cinese Tradizionale", "de"=>"Tedesco", "fr"=>"Francese",);
+        
         // Sorts array by key without change them.
         Ksort($languages);        
     ?>
     
     <div class = "body">
 
-        <div class="intestazione">
-            <img class="logo" src="image/logo.png" width="300" height="47" alt="logo"/>        
+        <div class = "intestazione">
+           <img class = "logo" src = "image/logo.png" width = "300" height = "47" alt = "logo"/>
         </div>
 
         <!-- The name of web page -->
         <p class = "titolo">Traduttore</p>
 
-        <!-- All content iso send in POST request -->
-        <form action = "translate.php" method = "POST">
+        <!-- All content is send in POST request -->
+        <form id ="trad" name = 'trad' action = "translate.php" method = "POST">
 
             <!-- Select of the source language -->
             <select class = "source_lang" name = "source_lang">
                 
-                <!-- PHP's foreach to view the languages aviable for translate -->
-                <?php foreach($languages as $code => $lang) { ?>
-                <option value = "<?php echo $code; ?>"><?php echo $lang; ?></option>
-                <?php } ?>
-            
+                <!-- PHP's function to view the languages aviable for translate -->
+                <?php if ($_SESSION['source_lang'] == NULL) 
+                      {
+                          foreach($languages as $code => $lang) 
+                          { ?>
+                              <option value = "<?php echo $code; ?>"> <?php echo $lang; ?> </option>
+                    <?php }
+                      } 
+                      else 
+                      {
+                          foreach($languages as $code => $lang) 
+                          { if ($code == $_SESSION['source_lang'])
+                            {?>
+                                <option value = "<?php echo $code; ?>"> <?php echo $lang; ?> </option>
+                      <?php }
+        
+                          }
+                          foreach($languages as $code => $lang) 
+                          {?>
+                              <option value = "<?php echo $code; ?>"> <?php echo $lang; ?> </option>
+                    <?php }
+                      } ?>           
+     
             </select>
-            
            
             <!-- Insert text to translate -->
-           	<textarea class = "source_text" name = "source_text" onclick = "javascript:source_text.value = ''"><?php echo($inputStr);?></textarea>
+           	<textarea id = "source_text" class = "source_text" name = "source_text" onclick = "javascript:select(source_text)" onkeypress = "javascript:send(this, event);"><?php echo($inputStr); ?></textarea>
            
             <!-- Select destination language -->
-            <select class = "dest_lang" name = "dest_lang">
+            <select id = "dest_lang" class = "dest_lang" name = "dest_lang">
 
                 <!-- PHP's foreach to view the languages aviable for translate -->
-                <?php foreach($languages as $code => $lang) { ?>
-                <option value = "<?php echo $code; ?>"><?php echo $lang; ?></option>
-                <?php } ?>
+                <?php if ($_SESSION['source_lang'] == NULL) 
+                      {
+                          foreach($languages as $code => $lang) 
+                          { ?>
+                              <option value = "<?php echo $code; ?>"> <?php echo $lang; ?> </option>
+                    <?php }
+                      } 
+                      else 
+                      {
+                          foreach($languages as $code => $lang) 
+                          { if ($code == $_SESSION['dest_lang'])
+                            { ?>
+                                <option value = "<?php echo $code; ?>"> <?php echo $lang; ?> </option>
+                      <?php }
+        
+                          }
+                          foreach($languages as $code => $lang) 
+                          { ?>
+                              <option value = "<?php echo $code; ?>"> <?php echo $lang; ?> </option>
+                    <?php }
+                      }?>    
 
             </select>
-            
-                       
-            <input class = "submit" type = "submit" value = "Traduci">
+              
+            <input class = "traduci_sub" type = "submit" value = "Traduci">
         
         </form>
-           
+
+
+        <!-- All content is send in GET request -->
+        <form id = "speech" name = "speech" action = "translate.php" method = "GET">
+
+            <!-- View the text translated -->
+            <textarea id = "transl_text" class = "transl_text" name = "transl_text" onclick = "javascript:select(transl_text)"><?php echo($translatedStr); ?></textarea>
             
-        <!-- View the text translated -->
-        <textarea id = "transl_text" class = "transl_text" name = "transl_text" onclick = "javascript:transl_text.value = '' "><?php echo $translatedStr; ?></textarea>
+            <input class = "ascolta_sub" type = "submit" value="">
+
+        </form>
             
-        <!--<button class = "reset" onclick = "javascript:source_text.value = '', transl_text.value = '' ">Reset</button>-->
+        <button class = "reset" onclick = "javascript:trad.source_text.value = ''; javascript:speech.transl_text.value = ''">Reset</button>
+
+        <!--<audio autoplay="autoplay" controls="controls">
+        <source src="<?php $strResponse?>" type="audio/mp3" />
+        </audio>-->
 
         <div class="pagef">
             <img class="credit" src="image/credit.png" width="200" height="25" alt="credit"/>     
