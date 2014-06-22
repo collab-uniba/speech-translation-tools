b16a883f3b9d9eab9ef0ce74007ce6c89d102a70
deastis-s <deastis.salvatore@gmail.com>
2014-06-22 17:27:39 +0200

    text-to-speech implemented

--- translate.php
+++ translate.php
@@ -1,28 +1,39 @@
 <?php
 
-/* Initialize session for save the destination language value for text-to-speech */ 
+// Initialize session for save the destination language value for text-to-speech.
 session_start();
 
-if (isset($_POST["dest_lang"]))
+// Verify if there is a previous speech file.
+if (glob('./speech_file/*.mp3'))
 {
-    $_SESSION["speech_lang"] = $_POST["dest_lang"];
-    
+    $file = glob('speech_file/'.'*.mp3',GLOB_MARK);
+    foreach ($file as $file)
+    {
+        if (is_dir($file))
+        {
+            self::deleteDir($file);
+        }
+        else
+        {
+            unlink($file);
+        }
+    }
 }
 
 include 'HttpTranslator.php';
 include 'AccessTokenAuthentication.php';
 require 'credential.php';
 
-if('POST' == $_SERVER['REQUEST_METHOD'])
+if (isset($_POST['traduci_sub']))
 {
-    /* Session variable for keep the languages selected */
-    /* Sets session variable for source_lang */
+    // Session variable for keep the languages selected
+    // Sets session variable for source_lang
     if (isset($_POST['source_lang']))
     {
         $_SESSION['source_lang'] = $_POST['source_lang'];
     }
     
-    /* Sets session variable for dest_lang */
+    // Sets session variable for dest_lang.
     if (isset($_POST['dest_lang']))
     {
         $_SESSION['dest_lang'] = $_POST['dest_lang'];
@@ -50,9 +61,9 @@ if('POST' == $_SERVER['REQUEST_METHOD'])
         $category = 'general';
         
         // Variable that composes the string of parameters for the transaltion
-        $params = "text=".urlencode($inputStr)."&to=".$toLanguage."&from=".$fromLanguage;
+        $paramst = "text=".urlencode($inputStr)."&to=".$toLanguage."&from=".$fromLanguage;
         // URL to translate the text
-        $translateUrl = "http://api.microsofttranslator.com/v2/Http.svc/Translate?$params";
+        $translateUrl = "http://api.microsofttranslator.com/v2/Http.svc/Translate?$paramst";
     
         //Create the Translator Object.
         $translatorObj = new HTTPTranslator();
@@ -67,52 +78,35 @@ if('POST' == $_SERVER['REQUEST_METHOD'])
             $translatedStr = $val;
         }
 
+        $translatedText = urlencode($translatedStr);
+        $out = 'audio/mp3';
+        $params = "text=$translatedText&language=$toLanguage&format=$out";
+
+        //HTTP Speak method URL.
+        $url = "http://api.microsofttranslator.com/V2/Http.svc/Speak?$params";
+
+        $translatorObj = new HTTPTranslator();
+
+        $strResponse = $translatorObj -> curlRequest($url, $authHeader);
+        
+        //Create a fold to insert a speech file generated if not exists.
+        if (!is_dir('speech_file'))
+        {
+            mkdir('speech_file');
+        }
+        
+        //Create the name of speech file.
+        $var = uniqid('SPC_').".mp3";
+
+        //Save file into server directory.
+        file_put_contents('./speech_file/'.$var, $strResponse);
+
+
     } catch (Exception $e) 
       {
           echo "Exception: " . $e->getMessage() . PHP_EOL;
       }
 }
-elseif ('GET' == $_SERVER['REQUEST_METHOD'])
-{
-    try 
-    {
-        //Create the AccessTokenAuthentication object.
-        $authObj = new AccessTokenAuthentication();
-        //Get the Access token.
-        $accessToken  = $authObj->getTokens($grantType, $scopeUrl, $clientID, $clientSecret, $authUrl);
-        //Create the authorization Header string.
-        $authHeader = "Authorization: Bearer ". $accessToken;
-        
-        //Set the params.
-        $speechStr = urlencode($_GET["transl_text"]);
-        $language = $_SESSION["speech_lang"];
-        $out = 'audio/mp3';
-        $params = "text=$speechStr&language=$language&format=$out";
-        
-        //HTTP Speak method URL.
-        $url = "http://api.microsofttranslator.com/V2/Http.svc/Speak?$params";
-        
-        //Set the Header Content Type.
-        header('Content-Type: audio/mp3');
-        header('Content-Disposition: attachment; filename = '.uniqid('SPC_').'.mp3');
-        
-        //Create the Translator Object.
-        $translatorObj = new HTTPTranslator();
-    
-        //Call the curlRequest.
-        $strResponse = $translatorObj -> curlRequest($url, $authHeader);
-        echo $strResponse;
-     
-        //echo ('<audio autoplay = "autoplay" >  <source src = "'.$strResponse.'" type = "audio/mp3" /> </audio> ');
-        
-
-} catch (Exception $e) 
-  {
-      echo "Exception: " . $e->getMessage() . PHP_EOL;
-  }
-}
-
-//session_destroy();
 
 ?>
 
@@ -125,15 +119,14 @@ elseif ('GET' == $_SERVER['REQUEST_METHOD'])
     <meta charset="utf-8" />
     <title>Microsoft Translator</title>
     <link rel = "stylesheet" href = "css.css" />
-    <script type = "text/javascript" src="functions_js.js"></script>
+    <script type = "text/javascript" src = "functions_js.js"></script>
 </head>
 
 
 <body>
 
-
+    <!-- Declaration and sort of the array that contains the language of the translation. --> 
     <?php
-        // Declaration of the array that contains the language of the translation.
         $languages = array("a"=>"Seleziona lingua", "it"=>"Italiano", "en"=>"Inglese", "ja"=>"Giapponese", "pt"=>"Portoghese", "es"=>"Spagnolo", "tr"=>"Turco",
         "zh-CHS"=>"Cinese Semplificato", "zh-CHT"=>"Cinese Tradizionale", "de"=>"Tedesco", "fr"=>"Francese",);
         
@@ -147,16 +140,16 @@ elseif ('GET' == $_SERVER['REQUEST_METHOD'])
            <img class = "logo" src = "image/logo.png" width = "300" height = "47" alt = "logo"/>
         </div>
 
-        <!-- The name of web page -->
+        <!-- The name of web page. -->
         <p class = "titolo">Traduttore</p>
 
-        <!-- All content is send in POST request -->
+        <!-- All content is send in POST request. -->
         <form id ="trad" name = 'trad' action = "translate.php" method = "POST">
 
-            <!-- Select of the source language -->
+            <!-- Select of the source language. -->
             <select class = "source_lang" name = "source_lang">
                 
-                <!-- PHP's function to view the languages aviable for translate -->
+                <!-- PHP's function to view the languages aviable for translate. -->
                 <?php if ($_SESSION['source_lang'] == NULL) 
                       {
                           foreach($languages as $code => $lang) 
@@ -181,13 +174,13 @@ elseif ('GET' == $_SERVER['REQUEST_METHOD'])
      
             </select>
            
-            <!-- Insert text to translate -->
+            <!-- Insert text to translate. -->
            	<textarea id = "source_text" class = "source_text" name = "source_text" onclick = "javascript:select(source_text)" onkeypress = "javascript:send(this, event);"><?php echo($inputStr); ?></textarea>
            
-            <!-- Select destination language -->
+            <!-- Select destination language. -->
             <select id = "dest_lang" class = "dest_lang" name = "dest_lang">
 
-                <!-- PHP's foreach to view the languages aviable for translate -->
+                <!-- PHP's foreach to view the languages aviable for translate. -->
                 <?php if ($_SESSION['source_lang'] == NULL) 
                       {
                           foreach($languages as $code => $lang) 
@@ -211,34 +204,44 @@ elseif ('GET' == $_SERVER['REQUEST_METHOD'])
                       }?>    
 
             </select>
-              
-            <input class = "traduci_sub" type = "submit" value = "Traduci">
+            
+            
+            <!-- Traduci button. -->  
+            <input class = "traduci_sub" name ="traduci_sub" type = "submit" value = "Traduci">
         
         </form>
 
-
-        <!-- All content is send in GET request -->
-        <form id = "speech" name = "speech" action = "translate.php" method = "GET">
-
-            <!-- View the text translated -->
-            <textarea id = "transl_text" class = "transl_text" name = "transl_text" onclick = "javascript:select(transl_text)"><?php echo($translatedStr); ?></textarea>
             
-            <input class = "ascolta_sub" type = "submit" value="">
+        <!-- View the text translated. -->
+        <textarea id = "transl_text" class = "transl_text" name = "transl_text" onclick = "javascript:select(transl_text)"><?php echo $translatedStr; ?></textarea>
+           
+        
+        
+        <!-- Reset button. -->   
+        <button class = "reset" onclick = "javascript:trad.source_text.value = ''; javascript:transl_text.value = ''">Reset</button>
 
-        </form>
-            
-        <button class = "reset" onclick = "javascript:trad.source_text.value = ''; javascript:speech.transl_text.value = ''">Reset</button>
 
-        <!--<audio autoplay="autoplay" controls="controls">
-        <source src="<?php $strResponse?>" type="audio/mp3" />
-        </audio>-->
 
-        <div class="pagef">
-            <img class="credit" src="image/credit.png" width="200" height="25" alt="credit"/>     
+        <!-- PHP's script for read the name of the speech file. -->
+        <?php foreach(glob('./speech_file/*.*') as $filename)
+              {
+                  $speech = $filename;
+              }
+        ?>
+
+        <!-- Player for listen the text-to-speech. -->
+        <audio id = "t2s" controls = "controls" >
+            <source src = "<?php echo $speech; ?>" type = "audio/mpeg" />
+        </audio>
+
+
+
+        <!-- Page footer. -->
+        <div class = "pagef">
+            <img class = "credit" src = "image/credit.png" width = "200" height = "25" alt = "credit"/>     
         </div>
            
     </div>
 
 </body>
-
 </html>
