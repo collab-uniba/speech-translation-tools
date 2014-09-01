08a56b29878f230d502a954aa8c17a866541f44c
Fabio Calefato <calefato.uniba@gmail.com>
2014-09-01 17:42:17 +0200

    First commit with basic 1-1 chat example

---
+++ skype4java-example/src/it/uniba/di/SkypeSpike.java
@@ -0,0 +1,80 @@
+package it.uniba.di;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import com.skype.Call;
+import com.skype.CallListener;
+import com.skype.Chat;
+import com.skype.ChatMessage;
+import com.skype.ChatMessageListener;
+import com.skype.Skype;
+import com.skype.SkypeException;
+import com.skype.connector.Connector;
+import com.skype.connector.ConnectorException;
+
+public class SkypeSpike {
+	private Chat chat = null;
+
+	static {
+		Connector.useJNIConnector(true);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SkypeSpike test = new SkypeSpike();
+		test.connect();
+		String text = "";
+		String id = "harunicole"; // enter a skype id here to test the messaging
+									// function
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			text = br.readLine();
+			test.sendMessage(id, text);
+		}
+	}
+
+	private void sendMessage(String id, String text) throws SkypeException {
+		chat = (null == chat ? Skype.chat(id) : chat);
+		chat.send(text);
+	}
+
+	public void connect() throws Exception {
+		Connector.Status status = null;
+		Connector conn = Connector.getInstance();
+
+		try {
+			status = conn.connect();
+		} catch (ConnectorException e1) {
+			e1.printStackTrace();
+			throw new Exception(e1.getMessage());
+		}
+
+		if (status != Connector.Status.ATTACHED)
+			throw new Exception(new Exception(
+					"Please install Skype from www.skype.com and run it."));
+
+		// add Skype4Java listeners
+		try {
+			Skype.addChatMessageListener(chatMessageListener);
+		} catch (SkypeException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(Skype.getVersion());
+	}
+
+	private ChatMessageListener chatMessageListener = new ChatMessageListener() {
+
+		@Override
+		public void chatMessageSent(ChatMessage arg0) throws SkypeException {
+		}
+
+		@Override
+		public void chatMessageReceived(ChatMessage chatMessage)
+				throws SkypeException {
+			System.out.println(chatMessage.getSenderDisplayName() + "("
+					+ chatMessage.getSenderId() + "): "
+					+ chatMessage.getContent());
+		}
+	};
+}
