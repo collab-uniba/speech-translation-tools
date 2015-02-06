085c5e8f7eed2ec773a146b59f27a2f41d4773b8
Daniel Garcia <dano88@gmail.com>
2015-02-06 20:29:05 +0100

    splitting ClientTSForm. At line 44, it crashes

---
+++ client/src/lib/ClientTs.h
@@ -0,0 +1,39 @@
+#ifndef __CLIENTTS_H__
+#define __CLIENTTS_H__
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#include <wx/frame.h>
+#else
+#include <wx/wxprec.h>
+#endif
+
+#include <wx/sizer.h>
+#include <wx/wx.h>
+#include <wx/timer.h>
+#include <wx/stattext.h>
+#include <wx/richtext/richtextctrl.h>
+#include <wx/textctrl.h>
+#include <wx/button.h>
+#include <wx/grid.h>
+#include "NationList.h"
+#include "LoginWarnings.h"
+#include "../ArchiveLog.h"
+
+#include "..\GlobalVariables.h"
+#include "..\translateController\translateController.h"
+#include "..\translateController\translateVariable.h"
+#include "FrmMailSending.h"
+#include "FrmSettingMail.h"
+#include "AudioWizard.h"
+#include <list>
+#include "FrmSaveChat.h"
+
+#define MENU_ESCI 1800
+#define MENU_OPZIONI 1801
+#define MENU_SPEECH 1802
+#endif
